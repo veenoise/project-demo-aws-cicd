1d81b89f465ce684c1a2d5de58d00c9977ee9086
William Chua <william.eduard123@gmail.com>
2025-10-03 17:40:14 +0800

    Fix: path problems making script not read pm2

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /var/www/nextjs-app
 echo "Starting Next.js app..."
+
+export PATH=/home/ec2-user/.nvm/versions/node/v22.20.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/snapd/snap/bin
+
 pm2 start npm --name "nextjs-app" -- start
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Stopping Next.js app..."
+
+export PATH=/home/ec2-user/.nvm/versions/node/v22.20.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/snapd/snap/bin
+
 pm2 stop "nextjs-app" || true
