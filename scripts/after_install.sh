766d92f55849f1ec6e4436f4e8ac75a2f5944781
William Chua <william.eduard123@gmail.com>
2025-10-03 17:25:41 +0800

    Fix: afterinstall too complicated, simplified

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-# Define destination
-APP_DIR=/var/www/nextjs-app
-
-echo "Copying files to application directory..."
-cp -r /opt/codedeploy-agent/deployment-root/*/deployment-archive/* $APP_DIR
-
-echo "Setting permissions..."
-chown -R ec2-user:ec2-user $APP_DIR
-chmod -R 755 $APP_DIR
+cd /var/www/nextjs-app
+sudo chown -R ec2-user:ec2-user /var/www/nextjs-app
 
 echo "AfterInstall complete."
