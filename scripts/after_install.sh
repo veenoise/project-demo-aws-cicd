7302b53f9148e48427b31e62fb707d96c6e96723
William Chua <william.eduard123@gmail.com>
2025-10-03 22:31:42 +0800

    Fix: use pnpm in scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,4 @@ sudo chown -R ec2-user:ec2-user /var/www/nextjs-app
 
 export PATH=/home/ec2-user/.nvm/versions/node/v22.20.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/snapd/snap/bin
 
-npm install
-
 echo "AfterInstall complete."
