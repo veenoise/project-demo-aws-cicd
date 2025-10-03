e12950794d41b64910d2213a88eb9808a0b421a5
William Chua <william.eduard123@gmail.com>
2025-10-03 17:11:09 +0800

    Fix: errors in codedeploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd /var/www/nextjs-app
-echo "Installing dependencies..."
-npm install --production
+set -e
+
+# Define destination
+APP_DIR=/var/www/nextjs-app
+
+echo "Copying files to application directory..."
+cp -r /opt/codedeploy-agent/deployment-root/*/deployment-archive/* $APP_DIR
+
+echo "Setting permissions..."
+chown -R ec2-user:ec2-user $APP_DIR
+chmod -R 755 $APP_DIR
+
+echo "AfterInstall complete."
