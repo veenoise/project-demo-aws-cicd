60d993a19d95638a3f99a49a1d47c5093cec1dfc
William Chua <william.eduard123@gmail.com>
2025-10-03 23:30:00 +0800

    Fix: revert changes just increase instance type

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,5 @@ echo "Starting Next.js app..."
 
 export PATH=/home/ec2-user/.nvm/versions/node/v22.20.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/snapd/snap/bin
 
+pm2 stop "nextjs-app" || true
 pm2 start pnpm --name "nextjs-app" -- start
