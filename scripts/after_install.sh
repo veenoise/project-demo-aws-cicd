ba5967eb2d97c1d61ddb9352cf596495596df441
William Chua <william.eduard123@gmail.com>
2025-10-03 23:42:24 +0800

    Fix: no CI env

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,7 @@ cd /var/www/nextjs-app
 sudo chown -R ec2-user:ec2-user /var/www/nextjs-app
 
 export PATH=/home/ec2-user/.nvm/versions/node/v22.20.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/snapd/snap/bin
+export CI=true
 
 pnpm install
 
