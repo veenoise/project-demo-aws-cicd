cff5eac3edf65e54db16f4252c5349c693039f6f
William Chua <william.eduard123@gmail.com>
2025-10-03 23:32:37 +0800

    Fix: not prod

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,6 @@ sudo chown -R ec2-user:ec2-user /var/www/nextjs-app
 
 export PATH=/home/ec2-user/.nvm/versions/node/v22.20.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/snapd/snap/bin
 
-pnpm install --prod
+pnpm install
 
 echo "AfterInstall complete."
