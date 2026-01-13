0cadcbe09b78a1b861f0820a70b5e37ca9ee1360
William Chua <william.eduard123@gmail.com>
2026-01-13 16:38:16 +0800

    Fix: remove hidden file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Cleaning old files..."
 sudo rm -rf /var/www/nextjs-app/*
-sudo rm -rf /opt/codedeploy-agent/deployment-root/*
-sudo rm -rf /opt/codedeploy-agent/deployment-root/.*
+sudo rm -rf /var/www/nextjs-app/.*
