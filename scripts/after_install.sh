d99fa727cbf872fa1a3168ebb8c947c866fa92a7
William Chua <william.eduard123@gmail.com>
2026-01-13 13:24:33 +0800

    Fix: chown the dir afterinstall

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
+echo "Staring after_install script..."
+
 APP_DIR="/var/www/nextjs-app"
 USER_HOME="/home/ubuntu"
+USER="ubuntu"
 
 export HOME="$USER_HOME"
 export CI=true
 export NVM_DIR="$HOME/.nvm"
 export TMPDIR="$HOME/tmp"
+
+chown -R $USER:$USER "$APP_DIR"
 
 mkdir -p "$TMPDIR"
 
